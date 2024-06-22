68ac2a65927024d193faf72fa41559b4ca6fc66f
ProtonBS <95560567+ProtonBS@users.noreply.github.com>
2024-06-22 17:52:30 +0530

    Update infoTabs.jsx

--- src/app/anime/components/infoTabs.jsx
+++ src/app/anime/components/infoTabs.jsx
@@ -40,6 +40,20 @@ export default function DescriptionTabs({ data: data }) {
 						</CardBody>
 					</Card>
 				</Tab>
+<!--𝗦𝗵𝗮𝗿𝗲𝗨𝗦 𝗕𝗮𝗻𝗻𝗲𝗿 𝟭--><center>
+<script type="module" src="https://securepubads.shareusads.com/scripts/tag/js/sgpt.js" ></script>
+
+<div id="shareusadx-6739644104098893-3323887418">
+ <script>
+     window._shareustag = window._shareustag || {cmd:[]};
+    _shareustag.cmd.push(function() {
+      var slot1 = _shareustag.defineSlot('/shareusadx/6739644104098893/3323887418/AniEo Banner 1', ["Responsive"],'shareusadx-6739644104098893-3323887418');
+    });
+  </script>
+</div>
+<!--𝗦𝗵𝗮𝗿𝗲𝗨𝗦 𝗕𝗮𝗻𝗻𝗲𝗿 𝟭--></center>
+	
+				
 			</Tabs>
 		</div>
 	);
