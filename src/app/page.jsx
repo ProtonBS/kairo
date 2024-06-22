29479cd76ee319ed058291ee82b9262fe4d7c2b3
ProtonBS <95560567+ProtonBS@users.noreply.github.com>
2024-06-22 21:18:01 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -25,7 +25,7 @@ export default async function Home() {
 				{homePageCards("anime")}
 				{homePageCards("movies")}
 				{homePageCards("kdrama")}
-			//	{homePageCards("manga")}
+	
 				{homePageCards("web-series")}
 			</section>
 		</main>
