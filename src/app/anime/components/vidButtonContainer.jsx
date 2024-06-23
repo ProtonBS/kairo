634ef085c53d4f0c69469e50d332dc87dfddde81
ProtonBS <95560567+ProtonBS@users.noreply.github.com>
2024-06-23 09:39:38 +0530

    Update vidButtonContainer.jsx

--- src/app/anime/components/vidButtonContainer.jsx
+++ src/app/anime/components/vidButtonContainer.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from 'react';
 import { MediaPlayer, MediaProvider } from '@vidstack/react';
 import '@vidstack/react/player/styles/default/theme.css';
