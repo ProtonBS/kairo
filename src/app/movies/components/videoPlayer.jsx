da05ae2ca97bb63ffc00260526d5b984df4a4473
ProtonBS <95560567+ProtonBS@users.noreply.github.com>
2024-06-23 10:13:59 +0530

    Update videoPlayer.jsx

--- src/app/movies/components/videoPlayer.jsx
+++ src/app/movies/components/videoPlayer.jsx
@@ -1,29 +1,17 @@
-import { useEffect, useState } from 'react';
+const MovieVideoPlayer = async ({ id: id }) => {
+	const videoFrameGenerator = (id) => {
+		return (
+			<iframe
+				src={`https://vidsrc.pro/embed/movie/${id}`}
+				allowFullScreen
+				referrerPolicy="origin"
+				height={720}
+				className="w-full h-[240px] lg:h-[720px] rounded-lg"
+			></iframe>
+		);
+	};
 
-const MovieVideoPlayer = ({ id }) => {
-    const [isClient, setIsClient] = useState(false);
-
-    useEffect(() => {
-        setIsClient(true); // Set state to true when component mounts on client-side
-    }, []);
-
-    return (
-        <section className="flex flex-col items-center">
-            {isClient && (
-                <div className="mb-4">
-                    {/* Placeholder for any ad-related content */}
-                    {/* You can add an empty div or any other content here */}
-                </div>
-            )}
-            <iframe
-                src={`https://vidsrc.pro/embed/movie/${id}`}
-                allowFullScreen
-                referrerPolicy="origin"
-                height={720}
-                className="w-full h-[240px] lg:h-[720px] rounded-lg"
-            ></iframe>
-        </section>
-    );
+	return <section>{videoFrameGenerator(id)}</section>;
 };
 
 export default MovieVideoPlayer;
