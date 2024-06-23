d7b53ab046162623c202121181d2d53b41ad8db6
ProtonBS <95560567+ProtonBS@users.noreply.github.com>
2024-06-23 10:10:53 +0530

    Update videoPlayer.jsx

--- src/app/movies/components/videoPlayer.jsx
+++ src/app/movies/components/videoPlayer.jsx
@@ -1,25 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-const MovieVideoPlayer = async ({ id: id }) => {
-    const videoFrameGenerator = (id) => {
-        return (
-            <iframe
-                src={`https://vidsrc.pro/embed/movie/${id}`}
-                allowFullScreen
-                referrerPolicy="origin"
-                height={720}
-                className="w-full h-[240px] lg:h-[720px] rounded-lg"
-            ></iframe>
-        );
-    };
+const MovieVideoPlayer = ({ id }) => {
+    const [isClient, setIsClient] = useState(false);
 
     useEffect(() => {
+        setIsClient(true); // Set state to true when component mounts on client-side
+        // Load ad script dynamically
         const script = document.createElement('script');
         script.type = 'module';
         script.src = 'https://securepubads.shareusads.com/scripts/tag/js/sgpt.js';
+        script.async = true;
         document.body.appendChild(script);
 
         script.onload = () => {
+            // Once script is loaded, define the ad slot
             window._shareustag = window._shareustag || { cmd: [] };
             window._shareustag.cmd.push(function() {
                 var slot1 = window._shareustag.defineSlot(
@@ -27,14 +21,30 @@ const MovieVideoPlayer = async ({ id: id }) => {
                     ["Responsive"],
                     'shareusadx-6739644104098893-3323887418'
                 );
+                // Optionally, refresh ad slot if needed
+                // slot1.refresh();
             });
+        };
+
+        return () => {
+            // Clean up: remove the script from the document on component unmount
+            document.body.removeChild(script);
         };
     }, []);
 
+    // Render the component
     return (
         <section className="flex flex-col items-center">
-            <div id="shareusadx-6739644104098893-3323887418" className="mb-4"></div>
-            {videoFrameGenerator(id)}
+            {isClient && (
+                <div id="shareusadx-6739644104098893-3323887418" className="mb-4"></div>
+            )}
+            <iframe
+                src={`https://vidsrc.pro/embed/movie/${id}`}
+                allowFullScreen
+                referrerPolicy="origin"
+                height={720}
+                className="w-full h-[240px] lg:h-[720px] rounded-lg"
+            ></iframe>
         </section>
     );
 };
