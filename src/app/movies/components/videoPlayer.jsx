640e3862f955981bfa80228c262c547994b477c8
ProtonBS <95560567+ProtonBS@users.noreply.github.com>
2024-06-23 10:12:06 +0530

    Update videoPlayer.jsx

--- src/app/movies/components/videoPlayer.jsx
+++ src/app/movies/components/videoPlayer.jsx
@@ -5,38 +5,15 @@ const MovieVideoPlayer = ({ id }) => {
 
     useEffect(() => {
         setIsClient(true); // Set state to true when component mounts on client-side
-        // Load ad script dynamically
-        const script = document.createElement('script');
-        script.type = 'module';
-        script.src = 'https://securepubads.shareusads.com/scripts/tag/js/sgpt.js';
-        script.async = true;
-        document.body.appendChild(script);
-
-        script.onload = () => {
-            // Once script is loaded, define the ad slot
-            window._shareustag = window._shareustag || { cmd: [] };
-            window._shareustag.cmd.push(function() {
-                var slot1 = window._shareustag.defineSlot(
-                    '/shareusadx/6739644104098893/3323887418/AniEo Banner 1',
-                    ["Responsive"],
-                    'shareusadx-6739644104098893-3323887418'
-                );
-                // Optionally, refresh ad slot if needed
-                // slot1.refresh();
-            });
-        };
-
-        return () => {
-            // Clean up: remove the script from the document on component unmount
-            document.body.removeChild(script);
-        };
     }, []);
 
-    // Render the component
     return (
         <section className="flex flex-col items-center">
             {isClient && (
-                <div id="shareusadx-6739644104098893-3323887418" className="mb-4"></div>
+                <div className="mb-4">
+                    {/* Placeholder for any ad-related content */}
+                    {/* You can add an empty div or any other content here */}
+                </div>
             )}
             <iframe
                 src={`https://vidsrc.pro/embed/movie/${id}`}
