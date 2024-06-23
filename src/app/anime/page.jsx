ca769f87db464528ee9988969cc8befe2c8a0cc4
ProtonBS <95560567+ProtonBS@users.noreply.github.com>
2024-06-23 09:46:55 +0530

    Update page.jsx

--- src/app/anime/page.jsx
+++ src/app/anime/page.jsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 import styles from "../page.module.css";
 
 export const metadata = {
-	title: "Dramalama Anime",
-	description: "Anime page for Dramalama",
+	title: "Ani.MoM",
+	description: "Watch Animes On Ani.MoM",
 };
 
 import { top_airing, recent, popular } from "./data-fetch/request";
